ebb8083d4c56a86dd8ce841fa2ce23f61ce4594e
V-Karthick <karthickveeramani.dev@gmail.com>
2024-11-17 21:43:28 +0530

    Created project and added base ui to the website

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className='h-full w-full flex justify-center items-center bg-yellow-400'>
+        <div className='h-[90%] w-[90%] flex justify-between items-center'>
+            <div>Film Folio</div>
+            <div className='flex p-2 w-[20%] justify-between'>
+                <NavLink to="/login" className={({isActive})=>isActive ? "border-b-2 border-black" : ""}>Login</NavLink>
+                <NavLink to="/movies" className={({isActive})=>isActive ? "border-b-2 border-black" : ""}>Movies</NavLink>
+                <NavLink to="/analytics" className={({isActive})=>isActive ? "border-b-2 border-black" : ""}>Analytics</NavLink>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Movies.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MovieList from '../components/MovieList'
+
+const Movies = () => {
+  return (
+    <div className='h-full w-full'>
+      <MovieList />
+    </div>
+  )
+}
+
+export default Movies
